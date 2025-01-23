4ab3944a1ee501d0c70df626f067204633de1a60
Miguel Angel Jimenez Salazar <105370860+Maiki2002@users.noreply.github.com>
2025-01-23 02:33:22 -0600

    ky-001.js added

---
+++ Server/ky-001.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('sensores_data2.json')
+        .then(response => response.json())
+        .then(data => {
+            const dataDisplay = document.getElementById("KY-001");
+
+            const table = document.createElement("table");
+            const thead = document.createElement("thead");
+            const tbody = document.createElement("tbody");
+
+            const headerRow = document.createElement("tr");
+            const headers = ["Id", "Sensor", "Temperatura", "Hora"];
+            headers.forEach(headerText => {
+                const th = document.createElement("th");
+                th.textContent = headerText;
+                headerRow.appendChild(th);
+            });
+            thead.appendChild(headerRow);
+
+            data.forEach(sensorData => {
+                const row = document.createElement("tr");
+
+                const idCell = document.createElement("td");
+                idCell.textContent = sensorData.id;
+                row.appendChild(idCell);
+
+                const sensorCell = document.createElement("td");
+                sensorCell.textContent = sensorData.sensor;
+                row.appendChild(sensorCell);
+
+                const temperaturaCell = document.createElement("td");
+                temperaturaCell.textContent = sensorData.temperatura;
+                row.appendChild(temperaturaCell);
+
+                const timeCell = document.createElement("td");
+                const date = new Date(sensorData.timestamp);
+                timeCell.textContent = date.toLocaleString();
+                row.appendChild(timeCell);
+
+                tbody.appendChild(row);
+            });
+
+            table.appendChild(thead);
+            table.appendChild(tbody);
+
+            dataDisplay.appendChild(table);
+        })
+        .catch(error => console.error("Error fetching JSON data:", error));
+});
